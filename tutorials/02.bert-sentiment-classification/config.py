698044413702d3f8c13cdf06d90b8e3da332447e
msgi <mayuan120226@163.com>
2020-05-23 11:02:45 +0800

    Chinese sentiment classification using base bert model

--- tutorials/02.bert-sentiment-classification/config.py
+++ tutorials/02.bert-sentiment-classification/config.py
@@ -5,9 +5,9 @@ MAX_LEN = 512
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
 EPOCHS = 10
-BERT_PATH = "../input/bert_base_uncased/"
+BERT_PATH = "./input/bert_base_chinese/"
 MODEL_PATH = "model.bin"
-TRAINING_FILE = "../input/imdb.csv"
+TRAINING_FILE = "./input/sentiment.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(
     BERT_PATH,
     do_lower_case=True
--- tutorials/02.bert-sentiment-classification/train.py
+++ tutorials/02.bert-sentiment-classification/train.py
@@ -14,9 +14,6 @@ from model import BERTBaseUncased
 
 def run():
     dfx = pd.read_csv(config.TRAINING_FILE).fillna("NONE")
-    dfx.sentiment = dfx.sentiment.apply(
-        lambda x: 1 if x == "positive" else 0
-    )
 
     df_train, df_valid = model_selection.train_test_split(
         dfx,
